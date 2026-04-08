94587f6f997ec09aceca74f39877963b2b220262
gorushka99 <gorushka99@gmail.com>
2026-04-08 12:31:27 +0300

    Реализация команды version

--- lr1/dartpedia/cli/bin/cli.dart
+++ lr1/dartpedia/cli/bin/cli.dart
@@ -1,3 +1,8 @@
+const version = '3.11.4'; // Add this line
 void main(List<String> arguments) {
-  print('Привет, Dart!'); // Change this line
+  if (arguments.isEmpty) {
+    print('Hello, Dart!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  }
 }
