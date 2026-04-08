7ac204104205f8bcf9cbe548a266bb47a6da4db8
gorushka99 <gorushka99@gmail.com>
2026-04-08 23:06:58 +0300

    Изменение main в model_test.dart

--- lr1/dartpedia/wikipedia/test/model_test.dart
+++ lr1/dartpedia/wikipedia/test/model_test.dart
@@ -13,7 +13,7 @@ const String openSearchResponse = './test/test_data/open_search_response.json';
 void main() {
   group('deserialize example JSON responses from wikipedia API', () {
     test('deserialize Dart Programming Language page summary example data from '
-        'json file into a Summary object', () async {
+          'json file into a Summary object', () async {
       final String pageSummaryInput =
           await File(dartLangSummaryJson).readAsString();
       final Map<String, Object?> pageSummaryMap =
@@ -29,6 +29,16 @@ void main() {
           jsonDecode(articleJson) as Map<String, Object?>;
       final List<Article> articles = Article.listFromJson(articleMap);
       expect(articles.first.title.toLowerCase(), 'cat');
+    });
+
+    test('deserialize Open Search results example data from json file '
+        'into an SearchResults object', () async {
+      final String resultsString =
+          await File(openSearchResponse).readAsString();
+      final List<Object?> resultsAsList =
+          jsonDecode(resultsString) as List<Object?>;
+      final SearchResults results = SearchResults.fromJson(resultsAsList);
+      expect(results.results.length, greaterThan(1));
     });
   });
 }
