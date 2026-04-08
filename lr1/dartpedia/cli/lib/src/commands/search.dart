284a25a17ae6dfc076167d3b34de83c3354c3e17
gorushka99 <gorushka99@gmail.com>
2026-04-08 23:31:30 +0300

    Добавление функции "Мне везёт"

--- lr1/dartpedia/cli/lib/src/commands/search.dart
+++ lr1/dartpedia/cli/lib/src/commands/search.dart
@@ -6,7 +6,13 @@ import 'package:logging/logging.dart';
 import 'package:wikipedia/wikipedia.dart';
 
 class SearchCommand extends Command {
-  SearchCommand({required this.logger});
+  SearchCommand({required this.logger}) {
+    addFlag(
+      'im-feeling-lucky',
+      help:
+          'If true, prints the summary of the top article that the search returns.',
+    );
+  }
 
   final Logger logger;
 
@@ -35,6 +41,19 @@ class SearchCommand extends Command {
 
     final buffer = StringBuffer('Search results:');
     final SearchResults results = await search(args.commandArg!);
+
+    if (args.flag('im-feeling-lucky')) {
+      final title = results.results.first.title;
+      final Summary article = await getArticleSummaryByTitle(title);
+      buffer.writeln('Lucky you!');
+      buffer.writeln(article.titles.normalized.titleText);
+      if (article.description != null) {
+        buffer.writeln(article.description);
+      }
+      buffer.writeln(article.extract);
+      buffer.writeln();
+      buffer.writeln('All results:');
+    }
 
     for (var result in results.results) {
       buffer.writeln('${result.title} - ${result.url}');
