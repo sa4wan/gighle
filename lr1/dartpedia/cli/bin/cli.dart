866b5d167082d5da1265f9f669150e99646582f3
gorushka99 <gorushka99@gmail.com>
2026-04-08 12:35:50 +0300

    Интегрирование команды search в main

--- lr1/dartpedia/cli/bin/cli.dart
+++ lr1/dartpedia/cli/bin/cli.dart
@@ -1,13 +1,17 @@
 const version = '3.11.4'; // Add this line
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); // Change this from 'Hello, Dart!'
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    print('Search command recognized!');
   } else {
-    printUsage(); // Catch-all for any unrecognized command.
+    printUsage();
   }
 }
+
 
 
 void printUsage() { // Add this new function
