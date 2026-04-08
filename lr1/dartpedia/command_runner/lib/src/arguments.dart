4fd7b87b1d7b7054ae50beb9bb1559e79cb43994
gorushka99 <gorushka99@gmail.com>
2026-04-08 13:01:50 +0300

    Определение abstract class

--- lr1/dartpedia/command_runner/lib/src/arguments.dart
+++ lr1/dartpedia/command_runner/lib/src/arguments.dart
@@ -1,1 +1,15 @@
 enum OptionType { flag, option }
+
+// Paste this new class below the enum you added
+abstract class Argument {
+  String get name;
+  String? get help;
+
+  // In the case of flags, the default value is a bool.
+  // In other options and commands, the default value is a String.
+  // NB: flags are just Option objects that don't take arguments
+  Object? get defaultValue;
+  String? get valueHelp;
+
+  String get usage;
+}
